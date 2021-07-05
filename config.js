d9b149cde5143a70b73c04c1bb06cec95cba5749
avbkaskdasf <86974224+avbkaskdasf@users.noreply.github.com>
2021-07-05 22:52:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     introTitle: 'Kiều xinh đẹp à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introDesc: `Trái đất vốn lạ thường,
+    Mà sao em cứ đi nhầm đường,
+    Lạc vào tim anh lẻ loi,
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng Kiều đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Kiều đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Sau một thời gian chúng ta tìm hiểu nhau, Dũng không biết tỏ tính như vậy có sớm quá không.Nhưng D cảm thấy mình đã đủ thương và đã đặt tình cảm dành cho Kiều rất nhiều, D muốn đây là một mối quan hệ đẹp có thể cùng nhau đi một chặn đường dài đến hết đời 😙',
+    desc: 'vậy bây giờ chúng ta chính thức làm người yêu của nhau nhé <3 ',
     btnYes: 'Vẫn cứ là thích Dũng <33',
     btnNo: 'Không, Dũng à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao Dũng lại yêu Kiều <3',
